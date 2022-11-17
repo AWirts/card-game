a8eb4f413cc15fd295815f4bc82637a59603c0e8
Mason0909 <mason.joseph8@gmail.com>
2022-11-17 13:56:19 -0500

    restart
    starting from here probably, card shows card values but picture doesnt work

---
+++ Assets/Scripts/PlayerDeck.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeck : MonoBehaviour
+{
+    public List<Card> deck = new List<Card> ();
+
+    public int x;
+    public int deckSize;
+    // Start is called before the first frame update
+    void Start()
+    {
+        x=0;
+        deckSize = 40;
+
+        for(int i=0; i<deckSize; i++)
+        {
+            x= Random.Range(1, 4);
+            deck.Add(CardDatabase.cardList[x]);
+        }
+    }
+
+}
---
+++ Assets/Scripts/ThisCard.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class ThisCard : MonoBehaviour
+{
+    private List<Card> thisCard = new List<Card> ();
+    public int thisId;
+
+    public int id;
+    public string cardName;
+    public string cardDescription;
+    public int cost;
+    public int stamina;
+    public int attack;
+    public int health;
+
+    public Sprite thisSprite;
+
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI costText;
+    public TextMeshProUGUI staminaText;
+    public TextMeshProUGUI attackText; 
+    public TextMeshProUGUI healthText;
+    public TextMeshProUGUI descriptionText; 
+
+    public Image thatImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisCard.Add(CardDatabase.cardList[thisId]);
+       
+    }
+
+    void Update()
+    {
+        if (id != thisId)
+        {
+            thisCard [0] = CardDatabase.cardList[thisId];
+        }
+        id = thisCard[0].id;
+        cardName = thisCard[0].cardName;
+        cost=thisCard[0].cost;
+        stamina=thisCard[0].stamina;
+        attack=thisCard[0].attack;
+        health=thisCard[0].health;
+        cardDescription=thisCard[0].cardDescription;
+        thisSprite=thisCard[0].thisImage;
+
+        nameText.text=""+cardName;
+        costText.text=""+cost;
+        staminaText.text=""+stamina;
+        attackText.text=""+attack;
+        healthText.text=""+health;
+        descriptionText.text=""+cardDescription;
+
+        thatImage.sprite=thisSprite;
+    }
+}
